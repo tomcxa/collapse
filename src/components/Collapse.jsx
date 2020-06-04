71b52d260d7b4b1166cbe97c98c6c73455fd203c
tomcxa <tomcxa@yandex.ru>
2020-06-04 11:09:18 +0300

    fix link href

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -4,13 +4,18 @@ import CollapseContainer from '../styled/CollapseContainer'
 import CollapseContent from '../styled/CollapseContent'
 
 const Collapse = (props) => {
+    function onClickHandler(event) {
+        event.preventDefault()
+        props.onExpandedChange()
+    }
+
     return (
         <CollapseContainer>
             <CollapseContent isExpanded={props.isExpanded}>
                 {props.children}
             </CollapseContent>
 
-            <a href='#' onClick={props.onExpandedChange}>
+            <a href='/' onClick={onClickHandler}>
                 {props.isExpanded ? props.collapsedLabel : props.expandedLabel}
             </a>
         </CollapseContainer>
