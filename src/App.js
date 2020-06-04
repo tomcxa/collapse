da2ffc646c5a777036430b1c2f1bbba41672d20b
tomcxa <tomcxa@yandex.ru>
2020-06-04 11:16:19 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
 
     return (
             <Collapse
-                // collapsedLabel='Open'
-                // expandedLabel='Close'
+                collapsedLabel='Close'
+                expandedLabel='Open'
                 isExpanded={isExpanded}
                 onExpandedChange={handleExpandedChange}
             >
